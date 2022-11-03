1d4356e9011fb9c55b9b9a13153423babc14523d
bu19akov <111774206+bu19akov@users.noreply.github.com>
2022-11-03 14:47:16 +0100

    initial commit

--- static_code_analyzer.py
+++ static_code_analyzer.py
@@ -7,10 +7,6 @@ import ast
 class StaticCodeAnalyzer:
 
     def __init__(self):
-        self.errors = ("S001 Too long", "S002 Indentation is not a multiple of four", "S003 Unnecessary semicolon",
-                       "S004 At least two spaces required before inline comments", "S005 TODO found",
-                       "S006 More than two blank lines used before this line", "S007 Too many spaces after param")
-        self.found_errors = dict()
         self.current_file = None
 
     def check_length(self, num, line):
